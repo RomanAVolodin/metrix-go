b2c3de7e41e7d9ae0ba44b4edfab36d14d2c3225
Roman Volodin <rozarioagro@gmail.com>
2024-06-22 20:46:07 +0300

    refactor done

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,112 +1,22 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"math/rand"
-	"net/http"
-	"reflect"
-	"runtime"
-	"strconv"
+	"github.com/RomanAVolodin/metrix-go/internal/config"
+	"github.com/RomanAVolodin/metrix-go/internal/entities"
 	"time"
 )
 
-type MetricForExport struct {
-	name       string
-	value      string
-	stringType string
-}
-
-func (m MetricForExport) String() string {
-	return m.name + ":" + m.value + ":" + m.stringType
-}
-
-type Poller struct {
-	PollCount   uint32
-	RandomValue float64
-	Metrics     []MetricForExport
-}
-
-func (p *Poller) fetchMetrics() {
-	var ms runtime.MemStats
-	runtime.ReadMemStats(&ms)
-	metrics := reflect.ValueOf(ms)
-
-	mfe := make([]MetricForExport, 0, metrics.NumField())
-
-	for i := 0; i < metrics.NumField(); i++ {
-		name := metrics.Type().Field(i).Name
-		value := metrics.Field(i)
-		var strValue, stringType string
-		switch value.Kind() {
-		case reflect.Uint32, reflect.Uint64:
-			strValue = strconv.FormatUint(value.Uint(), 10)
-			stringType = "counter"
-		case reflect.Float64:
-			strValue = strconv.FormatFloat(value.Float(), 'f', -1, 64)
-			stringType = "gauge"
-		default:
-			continue
-		}
-
-		mfe = append(mfe, MetricForExport{name, strValue, stringType})
-	}
-
-	p.RandomValue = rand.ExpFloat64()
-	p.PollCount++
-	mfe = append(mfe, MetricForExport{
-		name:       "PollCount",
-		value:      strconv.Itoa(int(p.PollCount)),
-		stringType: "counter",
-	})
-	mfe = append(mfe, MetricForExport{
-		name:       "RandomValue",
-		value:      strconv.FormatFloat(p.RandomValue, 'f', -1, 64),
-		stringType: "gauge",
-	})
-	p.Metrics = mfe
-}
-
-func (p *Poller) printMetrics() {
-	for _, m := range p.Metrics {
-		fmt.Println(m)
-	}
-	fmt.Println(len(p.Metrics))
-}
-
-func sendMetric(metric *MetricForExport) error {
-	urlString := fmt.Sprintf("http://localhost:8080/update/%s/%s/%s", metric.stringType, metric.name, metric.value)
-	resp, err := http.Post(urlString, "Content-Type: text/plain", nil)
-
-	if err != nil {
-		return err
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return errors.New("bad status code")
-	}
-
-	return nil
-}
-
-const (
-	pollInterval   = 2 * time.Second
-	reportInterval = 10 * time.Second
-)
-
 func main() {
-	poller := Poller{}
-	tickerPoll := time.NewTicker(pollInterval)
-	tickerReport := time.NewTicker(reportInterval)
+	poller := entities.Poller{}
+	tickerPoll := time.NewTicker(config.PollInterval)
+	tickerReport := time.NewTicker(config.ReportInterval)
 	for {
 		select {
 		case <-tickerPoll.C:
-			poller.fetchMetrics()
+			poller.FetchMetrics()
 		case <-tickerReport.C:
 			for _, metric := range poller.Metrics {
-				go sendMetric(&metric)
+				go metric.SendToServer()
 			}
 		}
 	}
---
+++ internal/config/config.go
@@ -0,0 +1,8 @@
+package config
+
+import "time"
+
+const (
+	PollInterval   = 2 * time.Second
+	ReportInterval = 10 * time.Second
+)
